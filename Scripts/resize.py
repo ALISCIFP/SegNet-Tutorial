2363dcf044e80a048c2ebcaf14c5bc7c8bb43b14
yifeili3 <liyifei.leo@gmail.com>
2016-09-18 00:00:37 -0500

    pipeline

---
+++ Scripts/resize.py
@@ -0,0 +1,15 @@
+import PIL
+import pandas
+import scipy
+import cv2
+from scipy import misc
+import numpy as np
+from PIL import Image
+
+for i in range(1,2001):
+    origin_image=cv2.imread('/home/zshen5/Data/ImageNet2016/ADEChallengeData2016/images/validation/ADE_val_0000'+ str('%04d' %i) +'.jpg',0)
+    img_pred = cv2.imread('/home/zshen5/Data/ImageNet2016/ADEChallengeData2016/predictions_camvid88k/img/ADE_val_0000'+ str('%04d' %i) +'.png',0)
+    img_anno = cv2.imread('/home/zshen5/Data/ImageNet2016/ADEChallengeData2016/annotations/validation/ADE_val_0000'+ str('%04d' %i) +'.png',0)
+    outimg = cv2.resize(img_pred, (origin_image.shape[1],origin_image.shape[0]),interpolation=cv2.INTER_NEAREST)
+    cv2.imwrite('/home/zshen5/Data/ImageNet2016/ADEChallengeData2016/predictions_camvid100k/img_ori_size/ADE_val_0000' + str('%04d' %i) +'.png',outimg)
+print 'success'
